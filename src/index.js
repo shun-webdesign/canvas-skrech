c060557a11c478c2bb98ba8f9670fb3f1a08d208
Shun <52568174+shunsuke110@users.noreply.github.com>
2022-10-02 11:20:29 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,101 @@
+const canvasSketch = require("canvas-sketch");
+const math = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
+const Color = require("canvas-sketch-util/color");
+const risoColors = require("riso-colors");
+
+const setting = {
+  dimensions: [1080, 1080],
+  animate: true
+};
+
+const sketch = ({ context, width, height }) => {
+  let x, y, w, h, fill, stroke, blend;
+  // let radius, angle, rx, ry;
+
+  const num = 80;
+  const degrees = -50;
+
+  const rects = [];
+
+  const rectColors = [
+    random.pick(risoColors),
+    random.pick(risoColors),
+    random.pick(risoColors)
+  ];
+
+  const bgColor = random.pick(rectColors).hex;
+
+  for (let i = 0; i < num; i++) {
+    x = random.range(0, width);
+    y = random.range(0, height);
+    w = random.range(200, 600);
+    h = random.range(40, 200);
+
+    fill = random.pick(rectColors).hex;
+    stroke = random.pick(rectColors).hex;
+
+    blend = random.value() > 0.5 ? "overlay" : "source-over";
+
+    rects.push({ x, y, w, h, fill, stroke, blend });
+  }
+
+  return ({ context, width, height }) => {
+    context.fillStyle = bgColor;
+    context.fillRect(0, 0, width, height);
+
+    rects.forEach((rect) => {
+      const { x, y, w, h, fill, stroke, blend } = rect;
+      let shadowColor;
+
+      context.save();
+      context.translate(x, y);
+      context.strokeStyle = stroke;
+      context.fillStyle = fill;
+      context.lineWidth = 10;
+
+      context.globalCompositeOperation = blend;
+
+      drawSkewedRect({ context, w, h, degrees });
+      shadowColor = Color.offsetHSL(fill, 0, 0, -20);
+      shadowColor.rgba[3] = 0.5;
+
+      context.shadowColor = Color.style(shadowColor.rgba);
+      context.shadowOffsetX = -10;
+      context.shadowOffsetY = 20;
+
+      context.fill();
+      context.shadowColor = null;
+      context.stroke();
+
+      context.globalCompositeOperation = "source-over";
+
+      context.lineWidth = 2;
+      context.strokeStyle = "black";
+      context.stroke();
+      context.restore();
+    });
+    context.save();
+    context.translate(width * 0.5, height * 0.5);
+    context.restore();
+  };
+};
+
+const drawSkewedRect = ({ context, w = 600, h = 200, degrees = -45 }) => {
+  const angle = math.degToRad(degrees);
+  const rx = Math.cos(angle) * w;
+  const ry = Math.sin(angle) * w;
+
+  context.save();
+  context.translate(rx * -0.5, (ry + h) * -0.5);
+  context.beginPath();
+  context.moveTo(0, 0);
+  context.lineTo(rx, ry);
+  context.lineTo(rx, ry + h);
+  context.lineTo(0, h);
+  context.closePath();
+  // context.stroke();
+  context.restore();
+};
+
+canvasSketch(sketch, setting);
